1075e05b18f01c5cd8a89dd520285b304b0904c0
mr-stezz <mstezy@gmail.com>
2015-07-28 15:38:07 +0100

    initial commit

---
+++ components/Root.jsx
@@ -0,0 +1,36 @@
+// components/Root.jsx
+var React = require('react')
+var Router = require('react-router')
+var RouteHandler = Router.RouteHandler
+var Header = require('./Header.jsx')
+
+var Root = React.createClass({
+  render: function () {
+    var initialProps = {
+      __html: safeStringify(this.props)
+    }
+
+    return (
+      <html>
+        <head>
+          <title>{this.props.title}</title>
+        </head>
+        <body>
+          <Header />
+          <RouteHandler {...this.props} />
+          <script
+            id='initial-props'
+            type='application/json'
+            dangerouslySetInnerHTML={initialProps} />
+          <script src='bundle.js' />
+        </body>
+      </html>
+    )
+  }
+})
+
+function safeStringify(obj) {
+  return JSON.stringify(obj).replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--')
+}
+
+module.exports = Root
